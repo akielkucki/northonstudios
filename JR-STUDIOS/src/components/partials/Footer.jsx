b65bd409aa53b1ef37b01528f594c2c82e9f3e7b
Jaun Raza <jaundev768@gmail.com>
2025-03-03 23:17:53 +0500

    Footer fixed

--- JR-STUDIOS/src/components/partials/Footer.jsx
+++ JR-STUDIOS/src/components/partials/Footer.jsx
@@ -40,8 +40,8 @@ const FuturisticFooter = () => {
     offset: ["start end", "end end"]
   });
 
-  const opacity = useTransform(scrollYProgress, [0.8, 1], [0, 1]);
-  const y = useTransform(scrollYProgress, [0.8, 1], [50, 0]);
+  const opacity = useTransform(scrollYProgress, [0, 1], [0, 1]);
+  const y = useTransform(scrollYProgress, [0, 1], [50, 0]);
 
   const [logoRef, logoInView] = useInView({
     triggerOnce: true,
@@ -229,7 +229,7 @@ const FuturisticFooter = () => {
                     </div>
                     <div>
                       <p className="text-gray-400 font-light">Email</p>
-                      <a href="mailto:team@jrstudios.co" className="text-white hover:text-purple-400 transition-colors">
+                      <a href="team@jrstudios.co" className="text-white hover:text-purple-400 transition-colors">
                         team@jrstudios.co
                       </a>
                     </div>
